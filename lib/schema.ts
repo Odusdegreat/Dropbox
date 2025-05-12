634d8b615f92f1dacaaef4aed4964e6b3fd8a51d
odusdegreat <tobiodubote@gmail.com>
2025-05-12 19:43:37 +0100

    added more codes into schema component

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,5 +1,13 @@
-import { pgTable, text, uuid, integer, boolean } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  text,
+  uuid,
+  integer,
+  boolean,
+  timestamp,
+} from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
+import { timestamp } from "drizzle-orm/gel-core";
 
 export const files = pgTable("files", {
   id: uuid("id").defaultRandom().primaryKey(),
@@ -11,5 +19,25 @@ export const files = pgTable("files", {
   type: text("type").notNull(), //"folder"
 
   // storage information
-  fileUrl: text("file_url"),
+  fileUrl: text("file_url").notNull, // url to access file
+  thumbnailUrl: text("thumbnail_url:"),
+
+  // Ownership
+  userId: text("user_id").notNull(),
+  parentId: uuid("parent_id"), // Parent folder if (null for root items)
+
+  // file/folder flags
+  isFolder: boolean("is_folder").default(false).notNull,
+  isStarred: boolean("is_starred").default(false).notNull(),
+  isTrash: boolean("is_trash").default(false).notNull(),
+
+  //Timestamps
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
+
+export const filesRelations = relations(files, ({ one, many }) => ({
+    parent:
+//relationship 
+    children:
+}));
