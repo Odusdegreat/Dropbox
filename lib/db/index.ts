eb6a82ef8de604bb7fcc0908827bda26fc475b3f
odusdegreat <tobiodubote@gmail.com>
2025-05-14 17:49:14 +0100

    created the schema folder

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,8 +1,12 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-
 import * as schema from "./schema";
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env.local" }); // Optional, only if needed here
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("Missing DATABASE_URL in environment");
+}
 const sql = neon(process.env.DATABASE_URL!);
 
 export const db = drizzle(sql, { schema });
