3a9c17c5065e6408696ab8ef2d1e619d5aed2bde
odusdegreat <tobiodubote@gmail.com>
2025-05-29 19:44:52 +0100

    added more contents to route.ts under the upload folder

--- app/api/files/upload/route.ts
+++ app/api/files/upload/route.ts
@@ -3,7 +3,7 @@ import { files } from "@/lib/db/schema";
 import { auth } from "@clerk/nextjs/server";
 import { and, eq } from "drizzle-orm";
 import ImageKit from "imagekit";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { v4 as uuidv4 } from "uuid";
 
 // imagekit credentials
@@ -13,13 +13,13 @@ const imagekit = new ImageKit({
   urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT || "",
 });
 
-export async function POST(request: NextRequest){
-
-    try{
-        const imagekit = new ImageKit({
-          publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY || "",
-          privateKey: process.env.IMAGEKIT_PRIVATE_KEY || "",
-          urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT || "",
-        });
+export async function POST(request: NextRequest) {
+  try {
+    const { userId } = await auth();
+    if (!userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
+    const formData = await request.formData();
+    formData.get("file") as File;
+  } catch (error) {}
 }
