fd8d8f6cc99cf4eff0ed75b046bb7f195be42641
odusdegreat <tobiodubote@gmail.com>
2025-06-24 19:51:29 +0100

    feat: created some folders, corrrected some errors and integrated the image auth api's

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,31 @@
 "use client";
 import type { ThemeProviderProps } from "next-themes";
 import { ThemeProviderProps as NextThemesProvider } from "next-themes";
+import { ImageKitProvider } from "imagekitio-next";
+
 export interface ProviderProps {
   children: React.ReactNode;
   themeProp?: ThemeProviderProps;
 }
 
-export function Providers({ children, ThemeProps }: ProviderProps) {
-  return <h1>{children}</h1>;
+const authenticator = async () => {
+  try {
+    const response = await fetch("/app/api/imagekit-auth");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Authentication error:", error);
+    throw error;
+  }
+};
+export function Providers({ children, themeProps }: ProviderProps) {
+  return (
+    <ImageKitProvider
+      authenticator={authenticator}
+      publicKey={process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY || ""}
+      urlEndpoint={process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT || ""}
+    >
+      {children}
+    </ImageKitProvider>
+  );
 }
