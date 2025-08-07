e7d7473619a6035d958f7941f593e84f627c87be
odusdegreat <tobiodubote@gmail.com>
2025-08-08 00:35:55 +0100

    feat: trying to use firebase for integration

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,17 +1,21 @@
-// lib/firebase.ts
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCd57Du2ClwAaQ-rsREL4wu5JfnhwDANRI",
-  authDomain: "dropbox-7781f.firebaseapp.com",
-  projectId: "dropbox-7781f",
-  storageBucket: "dropbox-7781f.appspot.com", // ← corrected `.app` to `.appspot.com`
-  messagingSenderId: "197131165373",
-  appId: "1:197131165373:web:c7d838c23791a1438e21d4",
-  measurementId: "G-SBMJJYHYXC",
+  apiKey: "AIzaSyB7iQmed4rkkAVJWNXL46olvRtPOSfBIPc",
+  authDomain: "sudo-coin.firebaseapp.com",
+  projectId: "sudo-coin",
+  storageBucket: "sudo-coin.firebasestorage.app",
+  messagingSenderId: "584524966814",
+  appId: "1:584524966814:web:f84be8ec93f98b6fc1048f",
+  measurementId: "G-8R8JE0CLHR",
 };
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-
-export const storage = getStorage(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
