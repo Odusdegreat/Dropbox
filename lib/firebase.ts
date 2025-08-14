7f916412546468033253c4543290df5b1e40e1f2
odusdegreat <tobiodubote@gmail.com>
2025-08-15 00:26:48 +0100

    fix: fixed some errors

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
+// firebaseConfig.ts
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB7iQmed4rkkAVJWNXL46olvRtPOSfBIPc",
   authDomain: "sudo-coin.firebaseapp.com",
@@ -13,9 +10,10 @@ const firebaseConfig = {
   storageBucket: "sudo-coin.firebasestorage.app",
   messagingSenderId: "584524966814",
   appId: "1:584524966814:web:f84be8ec93f98b6fc1048f",
-  measurementId: "G-8R8JE0CLHR",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Initialize Firebase Storage
+export const storage = getStorage(app);
